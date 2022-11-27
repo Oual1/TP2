01eb01d96a7b5f9ca51120736d594201f13d76ca
Ouala <wala.chaaben@etudiant-enit.utm.tn>
2022-11-27 19:32:05 +0100

    adding jenkis file

---
+++ script.groovy
@@ -0,0 +1,23 @@
+def buildImage(){
+    echo "building application"
+    
+    sh docker build -t tp2:1.0.0
+    }
+def  pushImage(){
+    echo"pushing image to docker hub"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', 
+    passwordVariable: '10827685A', 
+    usernameVariable: 'walach')
+    ])
+    [sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh docker push tp2:1.0.0
+    ]
+    
+}
+def deployApp(){
+    echo" deploy the application"
+    sh "echo $PASS | docker login -u $USER --password-stdin"
+    sh docker pull tp2:1.0.0
+    sh docker run tp2:1.0.0
+
+}
