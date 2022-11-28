6434e1403439bc4150ce1d689eb387d3ed03fd76
Ouala <wala.chaaben@etudiant-enit.utm.tn>
2022-11-28 13:54:09 +0100

    loulou

--- script.groovy
+++ script.groovy
@@ -1,8 +1,6 @@
 def buildImage(){
     echo "building application"
-    
-    sh docker build -t tp2:1.0.0
-    }
+    sh "docker build -t tp2:1.0.0"}
 def  pushImage(){
     echo"pushing image to docker hub"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', 
@@ -10,15 +8,15 @@ def  pushImage(){
     usernameVariable: 'walach')
     ])
     [sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh docker push tp2:1.0.0
+    sh "docker push tp2:1.0.0"
     ]
     
 }
 def deployApp(){
     echo" deploy the application"
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh docker pull tp2:1.0.0
-    sh docker run tp2:1.0.0
+    sh "docker pull tp2:1.0.0"
+    sh "docker run tp2:1.0.0"
 
 }
 
